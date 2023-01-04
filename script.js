7fe25aa4f8e0246f0ca01821d8cff94b4c013c06
Bob Klasen <robfklasen@gmail.com>
2023-01-04 00:19:56 -0600

    finished resume modal

--- script.js
+++ script.js
@@ -2,3 +2,28 @@
 /* --------------------------- MAIN --------------------------- */
 
 // Main Selectors
+
+const modalClose = document.querySelector('.modal-close');
+const modal = document.querySelector('.resume-modal');
+const modalCover = document.querySelector('.modal-cover');
+const resumeBtn = document.querySelector('.resume-btn');
+
+resumeBtn.addEventListener('click', function () {
+  console.log('test');
+  modal.classList.add('fadeIn');
+  modalCover.classList.add('fadeIn');
+  modalClose.classList.add('fadeIn');
+  modal.classList.remove('pointerEvents-none');
+  modalCover.classList.remove('pointerEvents-none');
+  modalClose.classList.remove('pointerEvents-none');
+});
+
+modalClose.addEventListener('click', function () {
+  console.log('test');
+  modal.classList.remove('fadeIn');
+  modalCover.classList.remove('fadeIn');
+  modalClose.classList.remove('fadeIn');
+  modal.classList.add('pointerEvents-none');
+  modalCover.classList.add('pointerEvents-none');
+  modalClose.classList.add('pointerEvents-none');
+});
