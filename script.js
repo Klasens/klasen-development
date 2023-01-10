cfcce213e0d4cee72c2cd698b4f26b32cc93614e
Bob Klasen <robfklasen@gmail.com>
2023-01-09 22:03:30 -0600

    Added close modal on esc key and outside click

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const modal = document.querySelector('.resume-modal');
 const modalCover = document.querySelector('.modal-cover');
 const resumeBtn = document.querySelector('.resume-btn');
 
-resumeBtn.addEventListener('click', function () {
-  console.log('test');
+resumeBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
   modal.classList.add('fadeIn');
   modalCover.classList.add('fadeIn');
   modalClose.classList.add('fadeIn');
@@ -26,4 +28,29 @@ modalClose.addEventListener('click', function () {
   modal.classList.add('pointerEvents-none');
   modalCover.classList.add('pointerEvents-none');
   modalClose.classList.add('pointerEvents-none');
+});
+modal.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+});
+
+modalCover.addEventListener('click', function () {
+  modal.classList.remove('fadeIn');
+  modalCover.classList.remove('fadeIn');
+  modalClose.classList.remove('fadeIn');
+  modal.classList.add('pointerEvents-none');
+  modalCover.classList.add('pointerEvents-none');
+  modalClose.classList.add('pointerEvents-none');
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    modal.classList.remove('fadeIn');
+    modalCover.classList.remove('fadeIn');
+    modalClose.classList.remove('fadeIn');
+    modal.classList.add('pointerEvents-none');
+    modalCover.classList.add('pointerEvents-none');
+    modalClose.classList.add('pointerEvents-none');
+  }
 });
