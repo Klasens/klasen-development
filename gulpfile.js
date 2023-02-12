5fb20e6480decf5be1f824589fb385c6a2a6ca8f
Klasens <57231452+Klasens@users.noreply.github.com>
2023-02-12 01:40:19 -0600

    Redesign entire KD.com

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'Use strict';
+// ==== Requirements ==== //
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const data = require('gulp-data');
+const fs = require('fs');
+const path = require('path');
+
+// ==== Defualt Task ==== //
+function defaultTask(cb) {
+  cb();
+}
+
+// ==== Build Index ==== //
+gulp.task('nunjucks-index', function () {
+  return (
+    gulp
+      // -- Collect Index
+      .src('pages/index.html')
+      // -- Compile partials fromcards
+      .pipe(
+        nunjucksRender({
+          path: ['cards'],
+        })
+      )
+      // -- Output files to chapters folder
+      .pipe(gulp.dest('./'))
+  );
+});
+
+exports.default = defaultTask;
