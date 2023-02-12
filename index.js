fc4de46f14831c55da46872ccb72a79784194a11
Bob Klasen <robfklasen@gmail.com>
2023-02-12 16:11:19 -0600

    updating modal container

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 const scrollTag = document.querySelector('.card__tag');
 const tagContent = document.querySelector('#founder');
 const scrollResume = document.querySelector('#resume');
-const resumeContent = document.querySelector('.modal__close');
+const resumeContent = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.modal__close');
+const body = document.querySelector('.u--center');
 
 modalClose.addEventListener('click', function (e) {
   e.preventDefault();
+  body.classList.remove('u--overflow--hidden');
   overlay.classList.remove('u--opacity--1');
   overlay.classList.add('u--pointer-events--none');
   modal.classList.remove('u--opacity--1');
@@ -27,6 +29,9 @@ scrollResume.addEventListener('click', function (e) {
   modal.classList.remove('u--pointer-events--none');
   modalClose.classList.add('u--opacity--1');
   modalClose.classList.remove('u--pointer-events--none');
+  setTimeout(() => {
+    body.classList.add('u--overflow--hidden');
+  }, 500);
 });
 
 scrollTag.addEventListener('click', function (e) {
